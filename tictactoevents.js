851be81edfc1059c2ffcf9e7b12a58cd558cbf74
Julio Moreno <julioc.morenob@gmail.com>
2021-04-26 23:25:22 -0400

    Enter logic finished

--- tictactoevents.js
+++ tictactoevents.js
@@ -1,26 +1,84 @@
+
 var ticTacToeMatrix = [
 ["","",""],
 ["","",""],
 [ "","",""]];
-var player=null;
 
-function currentPlayer(player)
-{
-  if (player===null){
-  player = "X";
-}
-  else if(player==="X"){
-  player= "O";
-}
-  else{
-    player = "X";
+const playerOne = 'X';
+const playerTwo = 'O';
+var currentPlayer="";
+
+function turn(){
+  if(currentPlayer===""){
+    currentPlayer = playerOne;
   }
-  return player;
+ else if(currentPlayer==='X'){
+    currentPlayer = playerTwo;
+  }
+  else{
+    currentPlayer = playerOne;
+  }
 }
 
 function onClickRow1Column1() {
-  ticTacToeMatrix[0][0] = currentPlayer(player);
-  document.getElementById("row1Column1").innerHTML=ticTacToeMatrix[0][0];
+  if(ticTacToeMatrix[0][0]===""){
+  turn();
+  ticTacToeMatrix[0][0] = currentPlayer;
+  document.getElementById("row1Column1").innerHTML=ticTacToeMatrix[0][0];}
 }
 
+function onClickRow1Column2() {
+  if(ticTacToeMatrix[0][1]===""){
+  turn();
+  ticTacToeMatrix[0][1] = currentPlayer;
+  document.getElementById("row1Column2").innerHTML=ticTacToeMatrix[0][1];}
+}
+function onClickRow1Column3() {
+  if(ticTacToeMatrix[0][2]===""){
+  turn();
+  ticTacToeMatrix[0][2] = currentPlayer;
+  document.getElementById("row1Column3").innerHTML=ticTacToeMatrix[0][2];}
+}
+
+function onClickRow2Column1() {
+  if(ticTacToeMatrix[1][0]===""){
+  turn();
+  ticTacToeMatrix[1][0] = currentPlayer;
+  document.getElementById("row2Column1").innerHTML=ticTacToeMatrix[1][0];}
+}
+
+function onClickRow2Column2() {
+  if(ticTacToeMatrix[1][1]===""){
+  turn();
+  ticTacToeMatrix[1][1] = currentPlayer;
+  document.getElementById("row2Column2").innerHTML=ticTacToeMatrix[1][1];}
+}
+
+function onClickRow2Column3() {
+  if(ticTacToeMatrix[1][2]===""){
+  turn();
+  ticTacToeMatrix[1][2] = currentPlayer;
+  document.getElementById("row2Column3").innerHTML=ticTacToeMatrix[1][2];}
+}
+
+function onClickRow3Column1() {
+  if(ticTacToeMatrix[2][0]===""){
+  turn();
+  ticTacToeMatrix[2][0] = currentPlayer;
+  document.getElementById("row3Column1").innerHTML=ticTacToeMatrix[2][0];}
+}
+
+function onClickRow3Column2() {
+  if(ticTacToeMatrix[2][1]===""){
+  turn();
+  ticTacToeMatrix[2][1] = currentPlayer;
+  document.getElementById("row3Column2").innerHTML=ticTacToeMatrix[2][1];}
+}
+
+function onClickRow3Column3() {
+  if(ticTacToeMatrix[2][2]===""){
+  turn();
+  ticTacToeMatrix[2][2] = currentPlayer;
+  document.getElementById("row3Column3").innerHTML=ticTacToeMatrix[2][2];}
+}
 
