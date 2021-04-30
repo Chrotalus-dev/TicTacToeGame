6c93abc202318566efaf2c76a82427725b30fd1b
Julio Moreno <julioc.morenob@gmail.com>
2021-04-29 22:30:49 -0400

    Winner Logic

--- tictactoevents.js
+++ tictactoevents.js
@@ -25,6 +25,7 @@ function onClickRow1Column1() {
   turn();
   ticTacToeMatrix[0][0] = currentPlayer;
   document.getElementById("row1Column1").innerHTML=ticTacToeMatrix[0][0];}
+  calculateWinner(ticTacToeMatrix);
 }
 
 function onClickRow1Column2() {
@@ -32,12 +33,14 @@ function onClickRow1Column2() {
   turn();
   ticTacToeMatrix[0][1] = currentPlayer;
   document.getElementById("row1Column2").innerHTML=ticTacToeMatrix[0][1];}
+  calculateWinner(ticTacToeMatrix);
 }
 function onClickRow1Column3() {
   if(ticTacToeMatrix[0][2]===""){
   turn();
   ticTacToeMatrix[0][2] = currentPlayer;
   document.getElementById("row1Column3").innerHTML=ticTacToeMatrix[0][2];}
+  calculateWinner(ticTacToeMatrix);
 }
 
 function onClickRow2Column1() {
@@ -45,6 +48,7 @@ function onClickRow2Column1() {
   turn();
   ticTacToeMatrix[1][0] = currentPlayer;
   document.getElementById("row2Column1").innerHTML=ticTacToeMatrix[1][0];}
+  calculateWinner(ticTacToeMatrix);
 }
 
 function onClickRow2Column2() {
@@ -52,6 +56,7 @@ function onClickRow2Column2() {
   turn();
   ticTacToeMatrix[1][1] = currentPlayer;
   document.getElementById("row2Column2").innerHTML=ticTacToeMatrix[1][1];}
+  calculateWinner(ticTacToeMatrix);
 }
 
 function onClickRow2Column3() {
@@ -59,6 +64,7 @@ function onClickRow2Column3() {
   turn();
   ticTacToeMatrix[1][2] = currentPlayer;
   document.getElementById("row2Column3").innerHTML=ticTacToeMatrix[1][2];}
+  calculateWinner(ticTacToeMatrix);
 }
 
 function onClickRow3Column1() {
@@ -66,6 +72,7 @@ function onClickRow3Column1() {
   turn();
   ticTacToeMatrix[2][0] = currentPlayer;
   document.getElementById("row3Column1").innerHTML=ticTacToeMatrix[2][0];}
+  calculateWinner(ticTacToeMatrix);
 }
 
 function onClickRow3Column2() {
@@ -73,6 +80,7 @@ function onClickRow3Column2() {
   turn();
   ticTacToeMatrix[2][1] = currentPlayer;
   document.getElementById("row3Column2").innerHTML=ticTacToeMatrix[2][1];}
+  calculateWinner(ticTacToeMatrix);
 }
 
 function onClickRow3Column3() {
@@ -80,5 +88,28 @@ function onClickRow3Column3() {
   turn();
   ticTacToeMatrix[2][2] = currentPlayer;
   document.getElementById("row3Column3").innerHTML=ticTacToeMatrix[2][2];}
+  calculateWinner(ticTacToeMatrix);
 }
 
+
+function calculateWinner() {
+  for(let i=0;i<=2;i++){
+  if(ticTacToeMatrix[i][0]!=="" && ticTacToeMatrix[i][0]===ticTacToeMatrix[i][1] && ticTacToeMatrix[i][0]===ticTacToeMatrix[i][2]){
+    document.getElementById("winner").innerHTML="The winner is "+ ticTacToeMatrix[i][0];
+    break;
+  }
+}
+  for(let j=0;j<=2;j++){
+    if(ticTacToeMatrix[0][j]!=="" && ticTacToeMatrix[0][j]===ticTacToeMatrix[1][j] && ticTacToeMatrix[0][j]===ticTacToeMatrix[2][j]){
+      document.getElementById("winner").innerHTML="The winner is "+ ticTacToeMatrix[0][j];
+      break;
+    }
+  }
+  if(ticTacToeMatrix[0][0]!=="" && ticTacToeMatrix[0][0]===ticTacToeMatrix[1][1] && ticTacToeMatrix[0][0]===ticTacToeMatrix[2][2]){
+    document.getElementById("winner").innerHTML="The winner is "+ ticTacToeMatrix[0][0];
+  }
+  if(ticTacToeMatrix[0][2]!=="" && ticTacToeMatrix[0][2]===ticTacToeMatrix[1][1] && ticTacToeMatrix[0][2]===ticTacToeMatrix[2][0]){
+   document.getElementById("winner").innerHTML="The winner is "+ ticTacToeMatrix[0][2];
+  }
+
+}
